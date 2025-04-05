8b1d2abcf30e56641448ca1426fde44bc7547521
fds7280 <bludfree0@gmail.com>
2025-04-05 23:26:56 +0530

    Add files via upload

---
+++ proto.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkfs.ext4 /dev/sda3 &&
+mkfs.fat -F 32 /dev/sda1 &&
+mkswap /dev/sda2 &&
+mount /dev/sda3 /mnt &&
+mkdir -p /mnt/boot/efi &&
+mount /dev/sda1 /mnt/boot/efi &&
+swapon /dev/sda2 &&
+pacstrap /mnt base linux-lts linux-firmware sof-firmware base-devel grub efibootmgr nano networkmanager sudo &&
+genfstab /mnt &&
+genfstab /mnt > /mnt/etc/fstab &&
+arch-chroot /mnt &&
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime &&
+hwclock --systohc &&
+
+# nano /etc/locale.gen [en_US.UTF-8 UTF-8] 
+sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/' /mnt/etc/locale.gen &&
+locale-gen &&
+
+# nano /etc/locale.conf [LANG=en_US.UTF-8]  
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
+# nano /etc/hostname 
+echo "archlinux" >> /etc/hostname &&
+
+passwd &&
+useradd -m -G wheel -s /bin/bash testo &&
+passwd testo &&
+
+# EDITOR=nano visudo [wheel all=(all) nopasswd: all]  
+sed -i 's/^#\("%wheel ALL=(ALL:ALL) ALL")/\1' /etc/sudoers &&
+
+systemctl enable NetworkManager &&
+grub-install /dev/sda &&
+grub-mkconfig -o /boot/grub/grub.cfg &&
+umount -a 
+
+
+
+
+
+
+
