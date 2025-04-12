9b4c17242f03a0a0f8878a549c2c819bd8cfe3ab
fds7280 <bludfree0@gmail.com>
2025-04-12 22:10:30 +0530

    Update proto.sh

--- proto.sh
+++ proto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkfs.ext4 /dev/vda3 &&
-mkfs.fat -F 32 /dev/vda1 &&
-mkswap /dev/vda2 &&
-mount /dev/vda3 /mnt &&
+mkfs.ext4 /dev/sda3 &&
+mkfs.fat -F 32 /dev/sda1 &&
+mkswap /dev/sda2 &&
+mount /dev/sda3 /mnt &&
 mkdir -p /mnt/boot/efi &&
-mount /dev/vda1 /mnt/boot/efi &&
-swapon /dev/vda2 &&
+mount /dev/sda1 /mnt/boot/efi &&
+swapon /dev/sda2 &&
 pacstrap /mnt base linux-lts linux-firmware sof-firmware base-devel grub efibootmgr nano networkmanager sudo &&
 genfstab /mnt &&
 genfstab /mnt > /mnt/etc/fstab 
