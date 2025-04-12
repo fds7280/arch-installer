d44e3984a7ea0abc4e1f9688780ef7269985a46d
fds7280 <bludfree0@gmail.com>
2025-04-12 18:26:49 +0530

    Update afterchroot.sh

--- afterchroot.sh
+++ afterchroot.sh
@@ -15,8 +15,8 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "archlinux" >> /etc/hostname &&
 
 passwd &&
-useradd -m -G wheel -s /bin/bash prin &&
-passwd prin &&
+useradd -m -G wheel -s /bin/bash prod &&
+passwd prod &&
 
 # EDITOR=nano visudo [wheel all=(all) nopasswd: all]  
 sed -i 's/^#\s*\(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
